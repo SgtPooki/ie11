5306ad7945174629c0169b02c0c0a6a61a688982
Russell Dempsey <rdempsey@nerdery.com>
2015-08-06 23:33:24 -0700

    Added server code to grab image data

---
+++ src/server/routes/images.js
@@ -0,0 +1,48 @@
+/**
+ *
+ * @author Russell Dempsey <SgtPooki@gmail.com>
+ */
+
+ var Promise = require('promise');
+ var request = require('request');
+ var _ = require('lodash');
+
+ var promiseHttpGet = function promiseHttpGet(url) {
+     return new Promise(function (resolve, reject) {
+         request(
+             url,
+             function (error, response, body) {
+                 if (error || response.statusCode !== 200) {
+                     return reject(error);
+                 }
+                 resolve(body);
+             }
+         );
+     });
+ };
+
+module.exports = function (request, response) {
+    var promises = [
+        promiseHttpGet('http://data.nbcnews.com/drone/getbyid?id=newscms/entry/364286&output=today'),
+        promiseHttpGet('http://data.nbcnews.com/drone/getbyid?id=newscms/entry/367081&output=today'),
+        promiseHttpGet('http://data.nbcnews.com/drone/getbyid?id=newscms/entry/364286&output=today')
+    ];
+
+    Promise.all(promises)
+        .then(function (results) {
+            var photos = _(results)
+                .map(JSON.parse)
+                .pluck('results')
+                .flatten()
+                .pluck('gallery')
+                .pluck('photos')
+                .flatten()
+                .value();
+
+            response.send(photos);
+        })
+        .catch(function (error) {
+            console.log('found error...');
+            console.log(error);
+        });
+};
--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var config = require('./config');
-var app = express();
+var request = require('request');
+var Promise = require('promise');
+var _ = require('lodash');
 
+var app = express();
 
 app.set('port', (process.env.PORT || config.server.port));
 
@@ -12,10 +15,12 @@ config.server.staticFolders.forEach(function (staticPath) {
 app.set('views', config.views.path);
 app.set('view engine', config.views.engine);
 
-app.get('/', function(request, response) {
+app.get('/', function (request, response) {
   response.render('pages/index');
 });
 
-app.listen(app.get('port'), function() {
+app.get('/images', require('./routes/images'));
+
+app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
 });
