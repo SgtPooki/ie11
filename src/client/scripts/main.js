5812ec62668a46c7cb35c67cdaeeef8dd939f1c0
Russell Dempsey <rdempsey@nerdery.com>
2015-08-08 15:44:42 -0500

    Use browserify for scripts task

--- src/client/scripts/main.js
+++ src/client/scripts/main.js
@@ -1,6 +1,8 @@
 (function() {
     'use strict';
 
+    require('./vendor/mousetrap.min');
+
     alert('test');
 
 }());
--- tasks/scripts.js
+++ tasks/scripts.js
@@ -5,11 +5,17 @@
  * @author Russell Dempsey <SgtPooki@gmail.com>
  */
 
+var browserify = require('gulp-browserify');
+
 var gulpTask = module.exports = {};
 
 gulpTask.dependencies = [];
 gulpTask.task = function(gulp, cb) {
 
-    return gulp.src('./src/client/scripts/**/*')
+    return gulp.src('./src/client/scripts/main.js')
+        .pipe(browserify({
+          insertGlobals : false,
+          debug : !!process.env.NODE_ENV
+        }))
         .pipe(gulp.dest('./public/scripts'));
 };
